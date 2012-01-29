64709c6a6c6c571dd80a607ac5dfa6f171219efd
Cam Pedersen <diffference@gmail.com>
2012-01-29 16:24:40 -0500

    [minor] readability

--- bin/burst.js
+++ bin/burst.js
@@ -8,19 +8,27 @@ if (!module.parent) {
   config.get(function (err, options) {
     if (err) throw new Error(err);
     var command = argv[0];
-    if (command === 'as') {
-      burst.as(options.redis, argv[1]);
-    } else if (command === 'to') {
-      burst.to(options.redis, argv[1], argv[2]);
-    } else if (command === 'into') {
-      if (typeof(argv[1]) === 'number') {
-        options.redis = {host: '127.0.0.1', port: argv[1]};
-      } else {
-        var pieces = argv[1].split(':');
-        options.redis = {host: pieces[0], port: pieces[1]||6379};
-      }
-      config.save(options, function () {});
-    }
+    switch (command) {
+
+      case 'as':
+        burst.as(options.redis, argv[1]);
+        break;
+
+      case 'to':
+        burst.to(options.redis, argv[1], argv[2]);
+        break;
+
+      case 'into':
+        if (typeof(argv[1]) === 'number') {
+          options.redis = {host: '127.0.0.1', port: argv[1]};
+        } else {
+          var pieces = argv[1].split(':');
+          options.redis = {host: pieces[0], port: pieces[1]||6379};
+        }
+        config.save(options, function () {});
+        break;
+
+    }      
   });
 }
 
