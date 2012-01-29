704500b55152ac91175dd583ed2b83021b33d126
Cam Pedersen <diffference@gmail.com>
2012-01-29 16:44:18 -0500

    [minor] add help text

--- bin/burst.js
+++ bin/burst.js
@@ -3,6 +3,8 @@
 var argv = require('optimist').argv._;
 var burst = require('../lib/burst');
 var config = require('../lib/config');
+var log = require('../lib/log');
+var colors = require('colors');
 
 if (!module.parent) {
   config.get(function (err, options) {
@@ -26,6 +28,21 @@ if (!module.parent) {
           options.redis = {host: pieces[0], port: pieces[1]||6379};
         }
         config.save(options, function () {});
+        break;
+
+      default:
+        log('burst');
+        log('Simple command-line file sharing for developers on the same network.');
+        log('help', ' ');
+        log('help', '  Connect to a Redis server:'.grey);
+        log('help', '    burst into 10.0.0.123:6379');
+        log('help', ' ');
+        log('help', '  Accept a file transfer:'.grey);
+        log('help', '    burst as ' + process.env.USER);
+        log('help', ' ');
+        log('help', '  Send a file:'.grey);
+        log('help', '    burst to cam ./funny_picture.jpg');
+        log('help', ' ');
         break;
 
     }      
